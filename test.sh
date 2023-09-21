adaca66d407ccd85d58dd93b4bcce5c4bb94aa6f
Graham Christensen <graham@grahamc.com>
2023-09-21 09:25:56 -0400

    .

--- test.sh
+++ test.sh
@@ -9,7 +9,7 @@ git commit -m "."
 git push origin master
 sleep 1
 
-nix run nixpkgs#gh -- workflow run -R grahamc/nix-starter-flakes publish-generic.yml --field tag=go-v0.1.0
+nix run nixpkgs#gh -- workflow run -R grahamc/nix-starter-flakes publish-generic.yml --field tag=go-v0.1.1695301070
 export tag=go-v0.1.$(date +%s)
 
 git tag "$tag"
